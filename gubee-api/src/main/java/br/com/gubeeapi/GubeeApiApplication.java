de62419a27c7260b185b7959fe697f5d26d52116
Daniel Junior <danjun48@gmail.com>
2019-10-06 01:53:11 -0300

    initial commit

---
+++ gubee-api/src/main/java/br/com/gubeeapi/GubeeApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gubeeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GubeeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GubeeApiApplication.class, args);
+	}
+
+}
---
+++ gubee-api/src/test/java/br/com/gubeeapi/GubeeApiApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.gubeeapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GubeeApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
