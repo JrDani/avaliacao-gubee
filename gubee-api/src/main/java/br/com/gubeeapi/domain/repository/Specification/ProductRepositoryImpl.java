67fdfad245577f386dc75a39c2fdce14a8d18bdf
Daniel Junior <danjun48@gmail.com>
2019-10-08 17:38:47 -0300

    Filtro para recurso Produto

--- gubee-api/src/main/java/br/com/gubeeapi/domain/repository/Specification/ProductRepositoryImpl.java
+++ gubee-api/src/main/java/br/com/gubeeapi/domain/repository/Specification/ProductRepositoryImpl.java
@@ -3,7 +3,6 @@ package br.com.gubeeapi.domain.repository.Specification;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import javax.persistence.EntityManager;
@@ -11,11 +10,13 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import br.com.gubeeapi.domain.model.Product;
+import br.com.gubeeapi.domain.model.Stack;
 import br.com.gubeeapi.domain.repository.ProductRepositoryQuery;
 
 public class ProductRepositoryImpl implements ProductRepositoryQuery {
@@ -28,10 +29,12 @@ public class ProductRepositoryImpl implements ProductRepositoryQuery {
 		
 		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
 		CriteriaQuery<Product> criteria = criteriaBuilder.createQuery(Product.class);
+		
 		Root<Product> root = criteria.from(Product.class);
 		
 		Predicate[] predicates = toPredicate(productFilter, criteriaBuilder, root);
-		criteria.where(predicates);
+		criteria.where(criteriaBuilder.or(predicates));
+		criteria.distinct(true);
 		
 		TypedQuery<Product> query = entityManager.createQuery(criteria);
 		
@@ -40,36 +43,39 @@ public class ProductRepositoryImpl implements ProductRepositoryQuery {
 	
 	public Predicate[] toPredicate(ProductFilter productFilter, CriteriaBuilder criteriaBuilder, Root<Product> root) {
 	
-		List<Predicate> predicates = new ArrayList<>();
+		List<Predicate> predicates = new ArrayList<>();		
 		
-		//https://stackoverflow.com/questions/42530677/jpa-criteria-builder-in-clause-query/42531461
-		
-		if (productFilter.getStack().length > 0) {
-			Expression<String> parentExpression = root.get("stack");
+		if (productFilter.getStack() != null && productFilter.getStack().length > 0) {
 			
-			Predicate parentPredicate = parentExpression.in(					
-					Arrays.asList(productFilter.getStack())
-					.stream()
-					.map(s -> s.toLowerCase())
-					.collect(Collectors.toList())
+			Join<Product, Stack> joinOptions = root.join("stack", JoinType.LEFT);
+			
+			predicates.addAll(Arrays.asList(productFilter.getStack())
+				.stream()
+				.map(s -> {
+					Predicate p = criteriaBuilder.like(criteriaBuilder.lower(joinOptions.get("name")), "%"+s.toLowerCase()+"%");
+					return p;
+				})
+				.collect(Collectors.toCollection(ArrayList::new))
 			);
-			predicates.add(parentPredicate);
+										
         } 
 		
-		if (productFilter.getTargetMarket().length > 0) {
-			Expression<String> parentExpression = root.get("targetMarket");
+		if (productFilter.getTargetMarket() != null && productFilter.getTargetMarket().length > 0) {
 			
-			Predicate parentPredicate = parentExpression.in(					
-					Arrays.asList(productFilter.getTargetMarket())
-					.stream()
-					.map(s -> s.toLowerCase())
-					.collect(Collectors.toList())
+			Join<Product, Stack> joinOptions = root.join("targetMarket", JoinType.LEFT);
+			
+			predicates.addAll(Arrays.asList(productFilter.getTargetMarket())
+				.stream()
+				.map(s -> {
+					Predicate p = criteriaBuilder.like(criteriaBuilder.lower(joinOptions.get("name")), "%"+s.toLowerCase()+"%");
+					return p;
+				})
+				.collect(Collectors.toCollection(ArrayList::new))
 			);
-			
-			predicates.add(parentPredicate);
         } 
-       		
-        return predicates.toArray(new Predicate[predicates.size()]);
+
+		
+		return predicates.toArray(new Predicate[predicates.size()]);
 	}
 
 	
