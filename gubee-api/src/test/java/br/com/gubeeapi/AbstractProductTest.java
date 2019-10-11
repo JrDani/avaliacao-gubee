c24fd895a61e4818937e0f5b80a2273b53cf119b
Daniel Junior <danjun48@gmail.com>
2019-10-11 00:50:24 -0300

    Teste unitario e de integracao para POST Product

--- gubee-api/src/test/java/br/com/gubeeapi/AbstractProductTest.java
+++ gubee-api/src/test/java/br/com/gubeeapi/AbstractProductTest.java
@@ -1,24 +1,73 @@
 package br.com.gubeeapi;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import br.com.gubeeapi.domain.model.Product;
+import br.com.gubeeapi.domain.model.Stack;
+import br.com.gubeeapi.domain.model.TargetMarket;
 
 public class AbstractProductTest {	
 	
-	protected String mapToJson(Object obj) throws JsonProcessingException {
-		ObjectMapper objectMapper = new ObjectMapper();
-		return objectMapper.writeValueAsString(obj);
+	protected final static List<Product> PRODUCTS = new ArrayList<>();
+	
+	public Product buildProduct() {
+	   Product product = new Product();
+	   Stack stack = new Stack();
+	   Stack stack2 = new Stack();
+	   List<Stack> stacks = new ArrayList<>();
+	   
+	   TargetMarket targetMarket = new TargetMarket();
+	   List<TargetMarket> tms = new ArrayList<>();
+	   
+	   product.setProductName("Daniel job");
+	   product.setDescription("Carrinho de compras integrador");	  
+	   
+	   stack.setId(1L);
+	   stack.setName("PHP 7");		   
+	   
+	   stack2.setId(2L);
+	   stack2.setName("Kibana");	   		   
+	   
+	   stacks.add(stack);
+	   stacks.add(stack2);   
+		   
+	   product.setStack(stacks);		   
+	   
+	   targetMarket.setId(1L);
+	   targetMarket.setName("Ecommerce");
+	  
+	   tms.add(targetMarket);
+	   
+	   product.setTargetMarket(tms);
+	   
+	   return product;
 	}
 	
-	protected <T> T mapFromJson(String json, Class<T> clazz)
-		throws JsonParseException, JsonMappingException, IOException {
-		      
-		ObjectMapper objectMapper = new ObjectMapper();
-		return objectMapper.readValue(json, clazz);
+	public void setId(Product product) {		
+		if(product.getId() == null) {
+			product.setId(1L);
+		}else {
+			product.setId(product.getId()+1L);
+		}
+			
 	}
+	
+	public void buildProdutos() {
+	
+		Product product = buildProduct();
+		setId(product);
+		PRODUCTS.add(product);		
 		
+	}
+	
+	public void buildProdutos(Integer n) {
+		for (int j = 0; j < n; j++) {
+			Product product = buildProduct();
+			setId(product);
+			PRODUCTS.add(product);
+		}
+		
+	}
 }
--- gubee-api/src/test/java/br/com/gubeeapi/ProductTest.java
+++ gubee-api/src/test/java/br/com/gubeeapi/ProductTest.java
@@ -1,11 +1,12 @@
 package br.com.gubeeapi;
 
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.when;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -17,8 +18,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 
 import br.com.gubeeapi.domain.model.Product;
 import br.com.gubeeapi.domain.model.Stack;
@@ -27,55 +31,39 @@ import br.com.gubeeapi.domain.service.ProductService;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
-public class ProductTest extends AbstractProductTest{
-	
+public class ProductTest extends AbstractProductTest {
+
 	@Autowired
 	private MockMvc mockMvc;
-	
+
 	@MockBean
 	protected ProductService productService;
-		
+
 	@Test
 	public void getProductsList() throws Exception {
-	   String uri = "/produtos";
-	 
-	   Product product = new Product();
-	   product.setId(1L);
-	   product.setProductName("Daniel job");
-	   product.setDescription("Carrinho de compras integrador");
-  
-	   Stack stack = new Stack();
-	   stack.setId(1L);
-	   stack.setName("PHP 7");		   
-	   Stack stack2 = new Stack();
-	   stack2.setId(2L);
-	   stack2.setName("Kibana");	   		   
-	   List<Stack> stacks = new ArrayList<>();
-	   stacks.add(stack);
-	   stacks.add(stack2);   
-		   
-	   product.setStack(stacks);
-	   
-	   TargetMarket targetMarket = new TargetMarket();
-	   targetMarket.setId(1L);
-	   targetMarket.setName("Ecommerce");
-	   List<TargetMarket> tms = new ArrayList<>();
-	   tms.add(targetMarket);
-	   
-	   product.setTargetMarket(tms);
-	   
-	   List<Product> products = new ArrayList<Product>();
-	   products.add(product);
-	   	
-       given(productService.listarProdutos()).willReturn(products);
-       
-       this.mockMvc.perform(get(uri))
-	       .andExpect(status().isOk())
-	       .andExpect(content().json("[{'id': 1,'productName': 'Daniel job','description': 'Carrinho de compras integrador','stack': [{'id':1, 'name': 'PHP 7'}, {'id':2, 'name': 'Kibana'}], 'targetMarket':[{'id':1, 'name': 'Ecommerce'}]}]"));
+		String uri = "/produtos";
+
+		buildProdutos();
+
+		given(productService.listarProdutos()).willReturn(AbstractProductTest.PRODUCTS);
+
+		this.mockMvc.perform(get(uri))
+		.andExpect(status().isOk())
+		.andExpect(content().json(
+				"[{'id': 1,'productName': 'Daniel job','description': 'Carrinho de compras integrador','stack': [{'id':1, 'name': 'PHP 7'}, {'id':2, 'name': 'Kibana'}], 'targetMarket':[{'id':1, 'name': 'Ecommerce'}]}]"));
 	}
-	
+
 	@Test
 	public void newProduct() throws Exception {
-	   //TODO test com post
+		String uri = "/produtos";
+
+		Product product = buildProduct();
+
+		when(this.productService.cadastrarProduto(product)).thenReturn(product);
+
+		this.mockMvc.perform(post(uri).contentType(MediaType.APPLICATION_JSON).content(
+				"{\"productName\": \"Daniel job\",\"description\": \"Carrinho de compras integrador\",\"stack\": [{\"id\":1, \"name\": \"PHP 7\"}, {\"id\":2, \"name\": \"Kibana\"}], \"targetMarket\":[{\"id\":1, \"name\": \"Ecommerce\"}]}"))
+				.andExpect(status().isCreated());
+
 	}
 }
